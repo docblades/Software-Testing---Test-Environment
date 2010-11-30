69ba9d0d06c85f1a407d8269a8a922fcf668b544
Christian Blades <christian.blades@docblades.com>
2010-11-30 17:27:13 -0500

    Made decision table blackbox example more clear

--- blackbox_decisiontable.py
+++ blackbox_decisiontable.py
@@ -2,7 +2,9 @@ import decision_table_stuff as testenv
 
 # Example
 # None is the same as a dash in the decision table
+
 test_cases = [
+    # (inc, dep, {action 1: [values True, False, or None], action 2: [values]})
     (1,
      0,
      {1: [True, False, False, False, False, False],
