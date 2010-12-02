a5a07dad999a6264a17e8756deab699521ca0be2
Brett Richards <brichar3@spsu.edu>
2010-12-02 16:40:48 -0500

    Added complete Decision Based Test Cases

--- blackbox_decisiontable.py
+++ blackbox_decisiontable.py
@@ -6,20 +6,145 @@ import decision_table_stuff as testenv
 
 test_cases = [
     # (inc, dep, {action 1: [values True, False, or None], action 2: [values]})
-    (1,
-     0,
-     {1: [True, False, False, False, False, False],
-      2: [False, False, False, False]}
+    (1, 
+     1, 
+     {1: [True, False, False, False, False, False, False],
+      2: [True, False, False, False]}
      ),
-    (20001,
+    (20001, 
      1,
-     {1: [False, True, False, False, False, False],
+     {1: [False, True, False, False, False, False, False],
       2: [True, False, False, False]}
      ),
     (30001,
+     1,
+     {1: [False, False, True, False, False, False, False],
+      2: [True, False, False, False]}
+     )
+    (40001,
+     1,
+     {1: [False, False, False, True, False, False, False],
+      2: [True, False, False, False]}
+     )
+    (50001,
+     1,
+     {1: [False, False, False, False, True, False, False],
+      2: [True, False, False, False]}
+     )
+    (90001,
+     1,
+     {1: [False, False, False, False, False, True, False],
+      2: [True, False, False, False]}
+     )
+    (0,
+     1,
+     {1: [False, False, False, False, False, False, True],
+      2: [True, False, False, False]}
+     )
+    (1, 
+     2, 
+     {1: [True, False, False, False, False, False, False],
+      2: [False, True, False, False]}
+     ),
+    (20001, 
      2,
-     {1: [False, False, True, None, None, None],
-      2: [False, True, None, None]}
+     {1: [False, True, False, False, False, False, False],
+      2: [False, True, False, False]}
+     ),
+    (30001,
+     2,
+     {1: [False, False, True, False, False, False, False],
+      2: [False, True, False, False]}
+     )
+    (40001,
+     2,
+     {1: [False, False, False, True, False, False, False],
+      2: [False, True, False, False]}
+     )
+    (50001,
+     1,
+     {1: [False, False, False, False, True, False, False],
+      2: [False, True, False, False]}
+     )
+    (90001,
+     1,
+     {1: [False, False, False, False, False, True, False],
+      2: [False, True, False, False]}
+     )
+    (0,
+     1,
+     {1: [False, False, False, False, False, False, True],
+      2: [False, True, False, False]}
+     )
+    (1, 
+     4, 
+     {1: [True, False, False, False, False, False, False],
+      2: [False, False, True, False]}
+     ),
+    (20001, 
+     4,
+     {1: [False, True, False, False, False, False, False],
+      2: [False, False, True, False]}
+     ),
+    (30001,
+     4,
+     {1: [False, False, True, False, False, False, False],
+      2: [False, False, True, False]}
+     )
+    (40001,
+     4,
+     {1: [False, False, False, True, False, False, False],
+      2: [False, False, True, False]}
+     )
+    (50001,
+     4,
+     {1: [False, False, False, False, True, False, False],
+      2: [False, False, True, False]}
+     )
+    (90001,
+     4,
+     {1: [False, False, False, False, False, True, False],
+      2: [False, False, True, False]}
+     )
+    (0,
+     4,
+     {1: [False, False, False, False, False, False, True],
+      2: [False, False, True, False]}
+     )
+    (1, 
+     0, 
+     {1: [True, False, False, False, False, False, False],
+      2: [False, False, False, True]}
+     ),
+    (20001, 
+     0,
+     {1: [False, True, False, False, False, False, False],
+      2: [False, False, False, True]}
+     ),
+    (30001,
+     0,
+     {1: [False, False, True, False, False, False, False],
+      2: [False, False, False, True]}
+     )
+    (40001,
+     0,
+     {1: [False, False, False, True, False, False, False],
+      2: [False, False, False, True]}
+     )
+    (50001,
+     0,
+     {1: [False, False, False, False, True, False, False],
+      2: [False, False, False, True]}
+     )
+    (90001,
+     0,
+     {1: [False, False, False, False, False, True, False],
+      2: [False, False, False, True]}
+     )
+    (0,
+     0,
+     {1: [False, False, False, False, False, False, True],
+      2: [False, False, False, True]}
      )
     ]
 
