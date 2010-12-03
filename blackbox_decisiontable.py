8c07358649346afd8000eee79bd523aeb9d07349
Christian Blades <christian.blades@docblades.com>
2010-12-02 19:20:20 -0500

    Fixed blackbox_decisiontable.py - missing some commas between test cases

--- blackbox_decisiontable.py
+++ blackbox_decisiontable.py
@@ -5,7 +5,6 @@ import decision_table_stuff as testenv
 # None is the same as a dash in the decision table
 
 test_cases = [
-    # (inc, dep, {action 1: [values True, False, or None], action 2: [values]})
     (1, 
      1, 
      {1: [True, False, False, False, False, False, False],
@@ -20,27 +19,27 @@ test_cases = [
      1,
      {1: [False, False, True, False, False, False, False],
       2: [True, False, False, False]}
-     )
+     ),
     (40001,
      1,
      {1: [False, False, False, True, False, False, False],
       2: [True, False, False, False]}
-     )
+     ),
     (50001,
      1,
      {1: [False, False, False, False, True, False, False],
       2: [True, False, False, False]}
-     )
+     ),
     (90001,
      1,
      {1: [False, False, False, False, False, True, False],
       2: [True, False, False, False]}
-     )
+     ),
     (0,
      1,
      {1: [False, False, False, False, False, False, True],
       2: [True, False, False, False]}
-     )
+     ),
     (1, 
      2, 
      {1: [True, False, False, False, False, False, False],
@@ -55,27 +54,27 @@ test_cases = [
      2,
      {1: [False, False, True, False, False, False, False],
       2: [False, True, False, False]}
-     )
+     ),
     (40001,
      2,
      {1: [False, False, False, True, False, False, False],
       2: [False, True, False, False]}
-     )
+     ),
     (50001,
      1,
      {1: [False, False, False, False, True, False, False],
       2: [False, True, False, False]}
-     )
+     ),
     (90001,
      1,
      {1: [False, False, False, False, False, True, False],
       2: [False, True, False, False]}
-     )
+     ),
     (0,
      1,
      {1: [False, False, False, False, False, False, True],
       2: [False, True, False, False]}
-     )
+     ),
     (1, 
      4, 
      {1: [True, False, False, False, False, False, False],
@@ -90,27 +89,27 @@ test_cases = [
      4,
      {1: [False, False, True, False, False, False, False],
       2: [False, False, True, False]}
-     )
+     ),
     (40001,
      4,
      {1: [False, False, False, True, False, False, False],
       2: [False, False, True, False]}
-     )
+     ),
     (50001,
      4,
      {1: [False, False, False, False, True, False, False],
       2: [False, False, True, False]}
-     )
+     ),
     (90001,
      4,
      {1: [False, False, False, False, False, True, False],
       2: [False, False, True, False]}
-     )
+     ),
     (0,
      4,
      {1: [False, False, False, False, False, False, True],
       2: [False, False, True, False]}
-     )
+     ),
     (1, 
      0, 
      {1: [True, False, False, False, False, False, False],
@@ -125,22 +124,22 @@ test_cases = [
      0,
      {1: [False, False, True, False, False, False, False],
       2: [False, False, False, True]}
-     )
+     ),
     (40001,
      0,
      {1: [False, False, False, True, False, False, False],
       2: [False, False, False, True]}
-     )
+     ),
     (50001,
      0,
      {1: [False, False, False, False, True, False, False],
       2: [False, False, False, True]}
-     )
+     ),
     (90001,
      0,
      {1: [False, False, False, False, False, True, False],
       2: [False, False, False, True]}
-     )
+     ),
     (0,
      0,
      {1: [False, False, False, False, False, False, True],
