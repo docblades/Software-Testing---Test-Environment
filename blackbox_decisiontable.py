9176b3fae9650d7bc638adaef4f7a87627f190ee
Brett Richards <brichar3@spsu.edu>
2010-12-02 21:06:56 -0500

    Decision Based Table Cases FIXED OMG

--- blackbox_decisiontable.py
+++ blackbox_decisiontable.py
@@ -5,43 +5,44 @@ import decision_table_stuff as testenv
 # None is the same as a dash in the decision table
 
 test_cases = [
-    (1, 
-     1, 
+    # (inc, dep, {action 1: [values True, False, or None], action 2: [values]})
+    (1,  # TEST CASE 1 0 < inc <= 20000
+     1,  # dep 1
      {1: [True, False, False, False, False, False, False],
       2: [True, False, False, False]}
      ),
-    (20001, 
-     1,
+    (20001, # TEST CASE 2 20000 < inc <= 30000
+     1,  #dep 1
      {1: [False, True, False, False, False, False, False],
       2: [True, False, False, False]}
      ),
-    (30001,
-     1,
+    (30001,  # TEST CASE 3 30000 < inc <= 40000
+     1,    #dep 1
      {1: [False, False, True, False, False, False, False],
       2: [True, False, False, False]}
-     ),
-    (40001,
+     ) 
+    (40001, # TEST CASE 4  40000 < inc <= 50000
      1,
      {1: [False, False, False, True, False, False, False],
       2: [True, False, False, False]}
-     ),
-    (50001,
-     1,
+     )
+    (50001, # Test case 5  50000 < inc <= 90000
+     1,  #dep 1
      {1: [False, False, False, False, True, False, False],
       2: [True, False, False, False]}
-     ),
-    (90001,
-     1,
-     {1: [False, False, False, False, False, True, False],
-      2: [True, False, False, False]}
-     ),
-    (0,
-     1,
+     )
+    (90001,  # Test case 6  90000 < inc
+     1,   #dep 1
      {1: [False, False, False, False, False, False, True],
       2: [True, False, False, False]}
-     ),
-    (1, 
-     2, 
+     )
+    (0,  #Test Case 7    inc = 0
+     1,  #dep = 1
+     {1: [False, False, False, False, False, True, False],
+      2: [True, False, False, False]}
+     )
+    (1,  # Test case 8  0 < inc <= 20000 
+     2,  # dep = 2
      {1: [True, False, False, False, False, False, False],
       2: [False, True, False, False]}
      ),
@@ -54,29 +55,29 @@ test_cases = [
      2,
      {1: [False, False, True, False, False, False, False],
       2: [False, True, False, False]}
-     ),
+     )
     (40001,
      2,
      {1: [False, False, False, True, False, False, False],
       2: [False, True, False, False]}
-     ),
+     )
     (50001,
      2,
      {1: [False, False, False, False, True, False, False],
       2: [False, True, False, False]}
-     ),
+     )
     (90001,
      2,
      {1: [False, False, False, False, False, False, True],
       2: [False, True, False, False]}
-     ),
+     )
     (0,
      2,
-     {1: [False, False, False, False, False, True, False],
+     {1: [False, False, False, False, False, True, ],
       2: [False, True, False, False]}
-     ),
-    (1, 
-     4, 
+     )
+    (1, #Test Case 15    0 < inc <= 20000
+     4, #dep =4
      {1: [True, False, False, False, False, False, False],
       2: [False, False, True, False]}
      ),
@@ -89,29 +90,29 @@ test_cases = [
      4,
      {1: [False, False, True, False, False, False, False],
       2: [False, False, True, False]}
-     ),
+     )
     (40001,
      4,
      {1: [False, False, False, True, False, False, False],
       2: [False, False, True, False]}
-     ),
+     )
     (50001,
      4,
      {1: [False, False, False, False, True, False, False],
       2: [False, False, True, False]}
-     ),
+     )
     (90001,
-     4,
-     {1: [False, False, False, False, False, True, False],
-      2: [False, False, True, False]}
-     ),
-    (0,
      4,
      {1: [False, False, False, False, False, False, True],
       2: [False, False, True, False]}
-     ),
-    (1, 
-     0, 
+     )
+    (0,
+     4,
+     {1: [False, False, False, False, False, True, False],
+      2: [False, False, True, False]}
+     )
+    (1, #Test Case 22  0 < inc <= 20000
+     0,  #dep = 0
      {1: [True, False, False, False, False, False, False],
       2: [False, False, False, True]}
      ),
@@ -124,25 +125,25 @@ test_cases = [
      0,
      {1: [False, False, True, False, False, False, False],
       2: [False, False, False, True]}
-     ),
+     )
     (40001,
      0,
      {1: [False, False, False, True, False, False, False],
       2: [False, False, False, True]}
-     ),
+     )
     (50001,
      0,
      {1: [False, False, False, False, True, False, False],
       2: [False, False, False, True]}
-     ),
+     )
     (90001,
      0,
-     {1: [False, False, False, False, False, True, False],
+     {1: [False, False, False, False, False, False, True],
       2: [False, False, False, True]}
-     ),
+     )
     (0,
      0,
-     {1: [False, False, False, False, False, False, True],
+     {1: [False, False, False, False, False, True, False],
       2: [False, False, False, True]}
      )
     ]
